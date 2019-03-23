2f50a5814d108928d771f4afec7ed33d7537deb6
taraldv <36439345+taraldv@users.noreply.github.com>
2019-03-23 04:19:51 +0100

    inital commit

---
+++ Makefile
@@ -0,0 +1,21 @@
+PROG = sjakkBot
+CC = g++
+CPPFLAGS = -Wall -ggdb -std=c++11
+DEPENDFLAGS += -std=c++11
+
+OBJS = benchmark.o bitbase.o bitboard.o endgame.o evaluate.o main.o \
+	material.o misc.o movegen.o movepick.o pawns.o position.o psqt.o \
+	search.o thread.o timeman.o tt.o uci.o ucioption.o syzygy/tbprobe.o
+
+all: $(PROG) .depend
+
+$(PROG): $(OBJS)
+	$(CC) -o $@ $(OBJS)
+
+.depend:
+	-@$(CC) $(DEPENDFLAGS) -MM $(OBJS:.o=.cpp) > $@ 2> /dev/null
+
+-include .depend
+
+clean:
+	rm -f *stackdump $(PROG) main.o
---
+++ main.cpp
@@ -0,0 +1,59 @@
+/*
+  Stockfish, a UCI chess playing engine derived from Glaurung 2.1
+  Copyright (C) 2004-2008 Tord Romstad (Glaurung author)
+  Copyright (C) 2008-2015 Marco Costalba, Joona Kiiski, Tord Romstad
+  Copyright (C) 2015-2019 Marco Costalba, Joona Kiiski, Gary Linscott, Tord Romstad
+
+  Stockfish is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Stockfish is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
+
+#include "bitboard.h"
+#include "position.h"
+#include "search.h"
+#include "thread.h"
+#include "tt.h"
+#include "uci.h"
+#include "syzygy/tbprobe.h"
+
+namespace PSQT {
+  void init();
+}
+
+int main() {
+  UCI::init(Options);
+  PSQT::init();
+  Bitboards::init();
+  Position::init();
+  Bitbases::init();
+  Search::init();
+  Pawns::init();
+  Threads.set(Options["Threads"]);
+  Search::clear(); // After threads are up
+
+  //simulate cmd inputs
+  char* testFen = (char*)"rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2";
+  char* fen = (char*)"fen";
+  char* position = (char*)"position";
+  char* argv[3];
+  argv[0] = position;
+  argv[1] = fen;
+  argv[2] = testFen;
+
+  UCI::loop(3,argv);
+
+  Threads.set(0);
+  return 0;
+}
